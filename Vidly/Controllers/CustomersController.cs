8780bd2923b7b8c35aca131a026e0d9197849f60
Grzegorz Macała <gmacala92@gmail.com>
2018-12-30 01:38:34 +0100

    30.12.2018 Customers are loaded from DB

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -10,29 +10,41 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ActionResult Index()
         {
 
-            var Customers = GetCustomers();
+            var Customers = _context.Customers.ToList();
+            //var Customers = GetCustomers();//wczytywanie bez bazy danych
            
             return View(Customers);
         }
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            //var customer = GetCustomers().SingleOrDefault(c => c.Id == id);//wczytywanie bez bazy danych
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
                 return HttpNotFound();
             return View(customer);
         }
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer> {
-               new Customer { Name = "Grzegorz", Id = 0 },
-               new Customer { Name = "Klaudia", Id = 1 },
-               new Customer { Name = "Aneta", Id = 2 },
-               new Customer { Name = "Marek", Id = 3 }
-               };
-        }
+        // wczytywanie bez używania bazy danych
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer> {
+        //       new Customer { Name = "Grzegorz", Id = 0 },
+        //       new Customer { Name = "Klaudia", Id = 1 },
+        //       new Customer { Name = "Aneta", Id = 2 },
+        //       new Customer { Name = "Marek", Id = 3 }
+        //       };
+        //}
     }
 }
